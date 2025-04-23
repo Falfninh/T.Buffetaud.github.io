378b1d0bdb732ed13eae84282e0c6f8089e46922
Falfninh <113891411+Falfninh@users.noreply.github.com>
2025-04-23 10:41:07 +0200

    Create ScrollToTop.js

---
+++ Projets/TECHCARE/scripts/ScrollToTop.js
@@ -0,0 +1,7 @@
+/*Script pour l'effet de défilement quand on clique sur la flèche et les options du nav-header*/
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }

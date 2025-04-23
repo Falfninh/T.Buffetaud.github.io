c00c445a1bed5feab4e065ab459105abbe7733db
Falfninh <113891411+Falfninh@users.noreply.github.com>
2025-04-23 10:41:00 +0200

    Create Caroussel-PGChanger.js

---
+++ Projets/TECHCARE/scripts/Caroussel-PGChanger.js
@@ -0,0 +1,15 @@
+/*Script pour changer Témoignage - PG.png (Le caroussel)*/
+function updateImage() {
+    const imgPG = document.querySelector(".img-pg");
+    if (window.innerWidth <= 768) {
+      imgPG.src = "/Projets/Site TECHCARE/style/img/Témoignage - none.png";
+    } else {
+      imgPG.src = "/Projets/Site TECHCARE/style/img/Témoignage - PG.png";
+    }
+  }
+
+  // Appel initial
+  updateImage();
+
+  // Réagir au redimensionnement de la fenêtre
+  window.addEventListener("resize", updateImage);
